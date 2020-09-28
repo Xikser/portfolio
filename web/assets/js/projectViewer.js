ede67f7c9e6259c34865e964ac59736588dd42b8
Xikser <Xikser@wp.pl>
2020-09-28 14:00:11 +0200

    Create file

---
+++ web/assets/js/projectViewer.js
@@ -0,0 +1,9 @@
+const viewerButton = document.querySelectorAll('.button--portfolio-info')
+
+for(const button of viewerButton) {
+    button.addEventListener('click', displayViewer)
+}
+
+function displayViewer() {
+    
+}
