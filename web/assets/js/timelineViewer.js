d0b4f0573aa9dbe6ad1ee0e12a5ca2dea779068d
Xikser <Xikser@wp.pl>
2020-09-28 14:00:25 +0200

    Create file

---
+++ web/assets/js/timelineViewer.js
@@ -0,0 +1,4 @@
+const timeline = document.querySelector('.timeline');
+const timelineBoxArray = timeline.querySelectorAll(':scope .timeline__box');
+const isActive = 'timeline__box-content--is-active'
+
