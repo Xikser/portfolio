31d41ef35fbd6b86ceaf38eaa855c762bc71fceb
Xikser <Xikser@wp.pl>
2020-09-29 18:06:57 +0200

    Create clipboard for contact

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -10,3 +10,41 @@ document.addEventListener('scroll', () => {
         siteMap.classList.remove('map--is-active');
     }
 });
+
+
+
+//contact clipboard
+const clipboards = document.querySelectorAll('.contact__box');
+
+for(const clipboard of clipboards) {
+    clipboard.addEventListener('click', copyText)
+}
+
+function copyText() {
+    const elem = this.querySelector(':scope .contact__clipboard');
+
+     elem.select();
+     document.execCommand('copy');
+     createTooltip(this)
+}
+
+function createTooltip(parent) {
+    let tooltip = document.createElement('span')
+    tooltip.classList.add('tooltip')
+    tooltip.classList.add('tooltip--visible')
+    tooltip.innerHTML = 'Copied!'
+
+    displayTooltip(parent, tooltip)
+}
+
+function displayTooltip(parent, tooltip) {
+   parent.appendChild(tooltip)
+
+   setTimeout(() => {
+       hideTooltip(parent, tooltip)
+   }, 1000)
+}
+
+function hideTooltip(parent, tooltip) {
+    parent.removeChild(tooltip)
+}
