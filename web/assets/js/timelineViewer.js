8e05d55db9400397535e288a6d1e645ae0e92c2e
Xikser <Xikser@wp.pl>
2020-09-29 18:49:16 +0200

    Delete unused file

--- web/assets/js/timelineViewer.js
+++
@@ -1,4 +0,0 @@
-const timeline = document.querySelector('.timeline');
-const timelineBoxArray = timeline.querySelectorAll(':scope .timeline__box');
-const isActive = 'timeline__box-content--is-active'
-
